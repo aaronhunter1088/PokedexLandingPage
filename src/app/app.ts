bd50b39b6216c8405392d8b9ca8f6b0a838371e0
Michael Ball <aaronhunter@live.com>
2026-01-13 17:55:30 -0600

    update landing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,14 +11,31 @@ import {
     MatCardTitle
 } from "@angular/material/card";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     selector: 'app-root',
-    imports: [NgOptimizedImage, MaterialModule, MatCardContent, MatCardImage, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardActions, MatCardFooter, MatSlideToggle],
+    imports: [NgOptimizedImage, MaterialModule, MatCardContent, MatCardImage, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardActions, MatCardFooter, MatSlideToggle, FormsModule],
     templateUrl: './app.html',
     standalone: true,
     styleUrl: './app.css'
 })
 export class App {
     protected readonly title = signal('PokedexLandingPage');
+
+    toggle1Checked = false;
+    toggle2Checked = false;
+    toggle3Checked = false;
+
+    updateMode1(checked: boolean): void {
+        this.toggle1Checked = checked;
+    }
+
+    updateMode2(checked: boolean): void {
+        this.toggle2Checked = checked;
+    }
+
+    updateMode3(checked: boolean): void {
+        this.toggle3Checked = checked;
+    }
 }
