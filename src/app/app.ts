cf60d9c43b205e2ca1e6e07398c4a9b953bdd114
Michael Ball <aaronhunter@live.com>
2026-01-15 01:06:47 -0600

    remove duplicate region name.

--- src/app/app.ts
+++ src/app/app.ts
@@ -19,21 +19,21 @@ export class App {
     protected readonly icon_shuffle = 'shuffle';
 
     private readonly regionsInfoMap: { [key: string]: string[] } = {
-        'Kanto': ['1kantoMap.png', 'Kanto', '#C0C0C0'],
-        'Johto': ['2johtoMap.png', 'Johto', '#C0C0C0'],
-        'Hoenn': ['3hoennMap.png', 'Hoenn', '#C0C0C0'],
-        'Sinnoh': ['41sinnohMap.png', 'Sinnoh', '#C0C0C0'],
-        'Ancient Sinnoh': ['42hisuiSinnohMap.png', 'Ancient Sinnoh', 'navajowhite'],
-        'Unova': ['5unovaMap.png', 'Unova', '#C0C0C0'],
-        'Kalos': ['6kalosMap.png', 'Kalos', '#C0C0C0'],
-        'Alola': ['7alolaMap.png', 'Alola', '#C0C0C0'],
-        'Galar': ['8galarMap.png', 'Galar', '#C0C0C0'],
-        'Paldea': ['9paldeaMap.png', 'Paldea', '#C0C0C0']
+        'Kanto': ['1kantoMap.png', '#C0C0C0'],
+        'Johto': ['2johtoMap.png', '#C0C0C0'],
+        'Hoenn': ['3hoennMap.png', '#C0C0C0'],
+        'Sinnoh': ['41sinnohMap.png', '#C0C0C0'],
+        'Ancient Sinnoh': ['42hisuiSinnohMap.png', '#FFDEAD'],
+        'Unova': ['5unovaMap.png', '#C0C0C0'],
+        'Kalos': ['6kalosMap.png', '#C0C0C0'],
+        'Alola': ['7alolaMap.png', '#C0C0C0'],
+        'Galar': ['8galarMap.png','#C0C0C0'],
+        'Paldea': ['9paldeaMap.png', '#C0C0C0']
     };
 
     protected readonly title = signal('PokedexLandingPage');
     protected readonly currentIcon = signal(this.icon_info);
-    protected readonly backgroundImage = signal(Object.keys(this.regionsInfoMap)[0][0]);
+    protected readonly backgroundImage = signal(Object.values(this.regionsInfoMap)[0][0]);
     protected readonly regionName = signal(Object.values(this.regionsInfoMap)[0][1]);
     protected readonly regionColor = signal(Object.values(this.regionsInfoMap)[0][2]);
 
@@ -84,12 +84,11 @@ export class App {
     toggleBackground(): void {
         const randomIndex = Math.floor(Math.random() * Object.entries(this.regionsInfoMap).length);
         const entry = Object.entries(this.regionsInfoMap)[randomIndex];
-        const regionNameKey = entry[1];
-        const image = regionNameKey[0];
-        const region = regionNameKey[1];
-        const color = regionNameKey[2];
+        const regionNameKey = entry[0];
+        const image = entry[1][0];
+        const color = entry[1][1];
         this.backgroundImage.set(image);
-        this.regionName.set(region);
+        this.regionName.set(regionNameKey);
         this.regionColor.set(color);
     }
 
