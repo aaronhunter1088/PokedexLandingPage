a1d3e4484f812f51c4cac06241db8413b4f76adf
Michael Ball <aaronhunter@live.com>
2026-02-17 19:32:32 -0600

    update dev urls to work with mobile

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,17 @@
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 export const environment = {
     production: false,
-
-    springBootAppUrl: 'http://localhost:4201/springboot/',
-    angularAppUrl: 'http://localhost:4202',
-    combinedAppUrl: 'http://localhost:4203',
-    swaggerUrl: 'http://localhost:4204/combined/swagger-ui.html'
+    get springBootAppUrl(): string {
+        return isMobile ? 'http://192.168.1.152:4201/springboot/' : 'http://localhost:4201/springboot/';
+    },
+    get angularAppUrl(): string {
+        return isMobile ? 'http://192.168.1.152:4202' : 'http://localhost:4202';
+    },
+    get combinedAppUrl(): string {
+        return isMobile ? 'http://192.168.1.152:4203' : 'http://localhost:4203';
+    },
+    get swaggerUrl(): string {
+        return isMobile ? 'http://192.168.1.152:4204/combined/swagger-ui.html' : 'http://localhost:4204/combined/swagger-ui.html';
+    },
 };
